5e9a8325e105314eaac400ad35515fe4c5da1e3a
germonado <germonado@github.com>
2021-11-05 00:10:24 +0900

    2021/11/05

--- Python_PS_Practice/24_Swap_Nodes_in_Pairs.py
+++ Python_PS_Practice/24_Swap_Nodes_in_Pairs.py
@@ -18,3 +18,44 @@ class Solution:
         if fast:
             slow.val, fast.val = fast.val, slow.val
         return head
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        if not head.next:
+            return head
+        root = prev = ListNode()
+        prev.next = head
+        while head and head.next:
+            b = head.next
+            # head direct 3
+            head.next = b.next
+            # b moves to front
+            b.next = head
+            prev.next = b
+            head = head.next
+            prev = prev.next.next
+            
+        return root.next
+
+# Recursive
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head and head.next:
+            p = head.next
+            head.next = self.swapPairs(p.next)
+            p.next = head
+            return p
+            
+        return head
